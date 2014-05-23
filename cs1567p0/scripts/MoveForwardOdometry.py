b46caf846ed5d1eaefe443f7fcbb35b6134b5b0f
rosie-pls <student@rosie.(none)>
2014-05-22 20:20:24 -0400

    derp

--- cs1567p0/scripts/MoveForwardOdometry.py
+++ cs1567p0/scripts/MoveForwardOdometry.py
@@ -5,26 +5,34 @@ from nav_msgs.msg import *
 from geometry_msgs.msg import *
 from cs1567p0.srv import *
 
+sub = None
+
 def odometry_callback(data):
     command = Twist()
     send_command = rospy.ServiceProxy('constant_command', ConstantCommand)
 
     print data.pose.pose
 
-    if(data.pose.pose.position.x < 1.0):
-        command.linear.x = 0.5
+    if(data.pose.pose.position.x <1.0):
+        command.linear.x = 0.3
         send_command(command)
     else:
         command.linear.x = 0.0
         send_command(command)
+        sub.unregister()
+
 
 
 def initialize():
     pub = rospy.Publisher('/mobile_base/commands/reset_odometry', Empty, queue_size=10)
-    rospy.Subscriber('/odom', Odometry, odometry_callback)
+    global sub
+    sub = rospy.Subscriber('/odom', Odometry, odometry_callback)
     rospy.init_node('MoveForwardOdometry', anonymous=True)
     rospy.wait_for_service('constant_command')
+    while pub.get_num_connections() < 1:
+        rospy.sleep(0.1)
     pub.publish(Empty())
+    print "DERRRRRRR?"
     rospy.spin()
     
 
