2eeb0d18babc7e320fb205e57a159f708591b449
rosie-pls <student@rosie.(none)>
2014-05-24 16:17:18 -0400

    Add square odometry to rosie

--- cs1567p0/scripts/MoveSquareOdometry.py
+++ cs1567p0/scripts/MoveSquareOdometry.py
@@ -4,24 +4,115 @@ from std_msgs.msg import *
 from nav_msgs.msg import *
 from geometry_msgs.msg import *
 from cs1567p0.srv import *
+from time import time
+from math import sqrt
+sub = None
+current_action = None
+current_goal = None
+chain = []
+action_chain = []
+action_start_time = 0
+
 
 def odometry_callback(data):
     command = Twist()
     send_command = rospy.ServiceProxy('constant_command', ConstantCommand)
 
-    if(data.pose.pose.position.x < 1.0):
-        command.linear.x = 0.5
+    global current_goal
+    global current_action
+    global action_start_time
+    print data.pose.pose
+
+    if current_goal(data.pose.pose):
+        current_action(command)
         send_command(command)
+    elif len(chain) > 0:
+        action_start_time = time()
+        command.linear.x = 0.0
+        command.angular.z = 0.0
+        send_command(command)
+        current_goal = chain.pop(0)
+        current_action = action_chain.pop(0)
     else:
         command.linear.x = 0.0
+        command.angular.z = 0.0
         send_command(command)
-
+        sub.unregister()
 
 def initialize():
     pub = rospy.Publisher('/mobile_base/commands/reset_odometry', Empty, queue_size=10)
-    rospy.Subscriber('/odom', Odometry, odometry_callback)
+
+    global action_start_time
+
+    rotate_90_goal = lambda pose: pose.orientation.z < sqrt(0.5)
+    move_forward_x = lambda pose: pose.position.x < 1.0
+    move_backward_x = lambda pose: pose.position.x > 0.0
+    move_forward_y = lambda pose: pose.position.y < 1.0
+    move_backward_y = lambda pose: pose.position.y > 0.0
+    rotate_180_goal = lambda pose: pose.orientation.z > 0
+    rotate_270_goal = lambda pose: pose.orientation.z < -sqrt(0.5)
+    rotate_360_goal = lambda pose: pose.orientation.z <= 0
+    null_goal = lambda pose: (time() - action_start_time) < 0.5 
+    
+    global chain
+    global current_goal
+    global current_action
+    global action_chain
+
+
+    def rotate_action(command):
+        command.angular.z = .3
+    def move_action(command):
+        command.linear.x = .2
+    def null_action(command):
+        command.linear.x = 0.0
+        command.angular.z = 0.0 
+
+    chain.append(move_forward_x)
+    chain.append(null_goal)
+    chain.append(rotate_90_goal)
+    chain.append(null_goal)
+    chain.append(move_forward_y)
+    chain.append(null_goal)
+    chain.append(rotate_180_goal)
+    chain.append(null_goal)
+    chain.append(move_backward_x)
+    chain.append(null_goal)
+    chain.append(rotate_270_goal)
+    chain.append(null_goal)
+    chain.append(move_backward_y)
+    chain.append(null_goal)
+    chain.append(rotate_360_goal)
+    chain.append(null_goal)
+    
+    action_chain.append(move_action)
+    action_chain.append(null_action)
+    action_chain.append(rotate_action)
+    action_chain.append(null_action)
+    action_chain.append(move_action)
+    action_chain.append(null_action)
+    action_chain.append(rotate_action)
+    action_chain.append(null_action)
+    action_chain.append(move_action)
+    action_chain.append(null_action)
+    action_chain.append(rotate_action)
+    action_chain.append(null_action)
+    action_chain.append(move_action)
+    action_chain.append(null_action)
+    action_chain.append(rotate_action)
+    action_chain.append(null_action)
+
+    current_goal = chain.pop(0)
+    current_action = action_chain.pop(0)
+
+    global sub
+    sub = rospy.Subscriber('/odom', Odometry, odometry_callback)
     rospy.init_node('MoveSquareOdometry', anonymous=True)
     rospy.wait_for_service('constant_command')
+    while pub.get_num_connections() < 1:
+        rospy.sleep(0.1)
+    pub.publish(Empty())
+
     pub.publish(Empty())
     rospy.spin()
     
