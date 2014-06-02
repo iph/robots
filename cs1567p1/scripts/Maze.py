ec3ba0a226033ceb9c3fcf4dbde3398cbcaa8131
Sean Myers <SeanMyers0608@gmail.com>
2014-06-02 17:57:33 -0400

    Maze now can detect paths to get places. Initialization is just testing that, pretty hardcoded at the moment..

--- cs1567p1/scripts/Maze.py
+++ cs1567p1/scripts/Maze.py
@@ -26,6 +26,7 @@ class Cell(object):
             if wall is OPEN:
                 open_walls.append(iter)
             iter += 1
+        return open_walls
 
     def set_wall(self, direction, status):
         """direction should be defined above{LEFT, RIGHT, UP, DOWN}, same for status {OPEN, CLOSED, UNKNOWN}"""
@@ -35,6 +36,8 @@ class Cell(object):
     def get_wall(self, direction):
         return self.walls[direction]
 
+    def is_fully_unknown(self):
+        return self.walls[0] is UNKNOWN and self.walls[1] is UNKNOWN and self.walls[2] is UNKNOWN and self.walls[3] is UNKNOWN
 
 class KnownMaze(object):
     """A mechanism to keep track of where rosie is in the maze, the current status of the maze
@@ -59,7 +62,7 @@ and the goal in the maze."""
         return self.get_cell(self.current_col, self.current_row)
 
     def move_rosie_forward(self):
-        """Moves rosie in the direction she is currently facing. Check walls before movement."""
+        """Moves rosie in the direction she is currently facing."""
         direction = self.current_direction
         if direction is UP:
             self.current_row -= 1
@@ -71,7 +74,7 @@ and the goal in the maze."""
             self.current_col += 1
 
 
-    def warp_position(self, new_position):
+    def warp_to_position(self, new_position):
         """Mostly will be used for debugging. """
         self.current_col ,self.current_row = new_position
 
@@ -90,19 +93,88 @@ and the goal in the maze."""
     def is_rosie_done(self):
         return self.current_row is self.goal_row and self.current_col is self.goal_col
 
-    def _build_rosie_wall(self):
-        """Not sure if needed yet, but building the initial known wall may be helpful"""
-        # Left wall
-        for i in range(self.max_rows):
-            self.set_cell_wall(0, i, LEFT, CLOSED)
-        # Right Wall
-        for i in range(self.max_rows):
-            self.set_cell_wall(self.max_cols - 1, i, RIGHT, CLOSED)
+    def get_adjacent_connected_positions(self, current_position):
+        """Returns positions of any cells that can be traversed from the current position.
+        Example: I'm in cell (0,0) and the only open neigbhors are below and to the right of me.
+        I would return: (1,0) and (0,1)
+        """
+        col, row = current_position
+        cell = self.get_cell(col, row)
+        neighbor_positions = []
+        open_neighbors = cell.get_open_walls()
+        for neighbor in open_neighbors:
+            neihbor_pos = None
+            if neighbor is UP:
+                neighbor_pos = (col, row - 1)
+            elif neighbor is DOWN:
+                neighbor_pos = (col, row + 1)
+            elif neighbor is LEFT:
+                neighbor_pos = (col - 1, row)
+            else:
+                neighbor_pos = (col + 1, row)
 
-        # Top wall
-        for i in range(self.max_cols):
-            self.set_cell_wall(i, 0, LEFT, CLOSED)
+            neighbor_positions.append(neighbor_pos)
+        return neighbor_positions
+        
+    def print_path_and_maze(self, path):
+        WALL_SYMBOLS = {
+            UP: {
+                OPEN:    "    +",
+                CLOSED:  "----+",
+                UNKNOWN: "....+"
+            },
+            DOWN: {
+                OPEN:    "    +",
+                CLOSED:  "----+",
+                UNKNOWN: "....+"
+            },
+            LEFT: {
+                OPEN:    " ",
+                CLOSED:  "|",
+                UNKNOWN: "."
+            },
+            RIGHT: {
+                OPEN:    " ",
+                CLOSED:  "|",
+                UNKNOWN: "."                
+            }
+            
+        }
 
+        ROSIE_DIRECTION = {
+            UP:    "....",
+            DOWN:  "(vv)",
+            RIGHT: "(>>)",
+            LEFT:  "(<<)"
+        }
+
+        s = "+"
+        for j in xrange(0, self.max_cols):
+            s += WALL_SYMBOLS[UP][self.grid[j][0].get_wall(UP)]
+        s += "\n"
+
+
+        for i in xrange(0, self.max_rows):
+            for _ in range(2):
+                s += WALL_SYMBOLS[LEFT][self.grid[0][i].get_wall(LEFT)]
+                for j in xrange(0, self.max_cols):
+                    # Middle rosie
+                    cur_pos = (j,i)
+                    if cur_pos in path:
+                        s += ROSIE_DIRECTION[self.current_direction]
+                    elif self.grid[j][i].is_fully_unknown():
+                        s += "////"
+                    else:
+                        s += "    "
+                    s += WALL_SYMBOLS[RIGHT][self.grid[j][i].get_wall(RIGHT)]
+                s += "\n"
+
+            # Do bottom part
+            s += "+"
+            for j in xrange(0, self.max_cols):
+                s += WALL_SYMBOLS[DOWN][self.grid[j][i].get_wall(DOWN)]
+            s += "\n"
+        return s        
 
     def print_maze(self):
         WALL_SYMBOLS = {
@@ -121,7 +193,6 @@ and the goal in the maze."""
                 CLOSED:  "|",
                 UNKNOWN: "."
             },
-
             RIGHT: {
                 OPEN:    " ",
                 CLOSED:  "|",
@@ -143,28 +214,19 @@ and the goal in the maze."""
         s += "\n"
 
 
-
         for i in xrange(0, self.max_rows):
-            s += WALL_SYMBOLS[LEFT][self.grid[0][i].get_wall(LEFT)]
-            for j in xrange(0, self.max_cols):
-                # Middle rosie
-                if self.current_col is j and self.current_row is i:
-                    s += ROSIE_DIRECTION[self.current_direction]
-                else:
-                    s += "    "
-                s += WALL_SYMBOLS[RIGHT][self.grid[j][i].get_wall(RIGHT)]
-            s += "\n"
-
-
-            s += WALL_SYMBOLS[LEFT][self.grid[0][i].get_wall(LEFT)]
-            for j in xrange(0, self.max_cols):
-                # Middle rosie
-                if self.current_col is j and self.current_row is i:
-                    s += ROSIE_DIRECTION[self.current_direction]
-                else:
-                    s += "    "
-                s += WALL_SYMBOLS[RIGHT][self.grid[j][i].get_wall(RIGHT)]
-            s += "\n"
+            for _ in range(2):
+                s += WALL_SYMBOLS[LEFT][self.grid[0][i].get_wall(LEFT)]
+                for j in xrange(0, self.max_cols):
+                    # Middle rosie
+                    if self.current_col is j and self.current_row is i:
+                        s += ROSIE_DIRECTION[self.current_direction]
+                    elif self.grid[j][i].is_fully_unknown():
+                        s += "////"
+                    else:
+                        s += "    "
+                    s += WALL_SYMBOLS[RIGHT][self.grid[j][i].get_wall(RIGHT)]
+                s += "\n"
 
             # Do bottom part
             s += "+"
@@ -172,7 +234,8 @@ and the goal in the maze."""
                 s += WALL_SYMBOLS[DOWN][self.grid[j][i].get_wall(DOWN)]
             s += "\n"
         return s
-k = KnownMaze(5, 5, (0,0), (5,5))
-k.get_cell(0, 0).set_wall(RIGHT, OPEN)
-print k.print_maze()
+
+
         
+
+
--- cs1567p1/scripts/MazeSolver.py
+++ cs1567p1/scripts/MazeSolver.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 import rospy
+import pdb
 from cs1567p1.srv import *
 from std_srvs.srv import * 
 
-LEFT = 0
-RIGHT = 1
-UP = 2
-DOWN = 3
+from Maze import KnownMaze, CLOSED, OPEN, UNKNOWN, LEFT, RIGHT, UP, DOWN
+known_maze = KnownMaze(5, 5, (0,0), (5,5))
 
 make_maze_service = None
 print_maze_service = None
@@ -16,33 +15,87 @@ constant_command_service = None
 def move_forward():
     return 1
 
-def turn_90_degrees():
-    return 1
+def update_room():
+    global known_maze
+    next_wall ={ UP: RIGHT, RIGHT: DOWN, DOWN: LEFT, LEFT: UP}
+    end_direction = known_maze.get_rosie_direction()
+    update_known_maze_wall()
+    current_direction = next_wall[end_direction]
+    known_maze.set_rosie_direction(current_direction)
+    while(current_direction is not end_direction):
+        update_known_maze_wall()
+        current_direction = next_wall[current_direction]
+        known_maze.set_rosie_direction(current_direction)
 
-def solve_maze():
-    return 1
 
+
+
+def update_known_maze_wall():
+    global known_maze
+    col, row = known_maze.get_rosie_position()
+    direction = known_maze.get_rosie_direction()
+    wall_bool = get_wall_service(col, row, direction)
+    wall_status = UNKNOWN
+    if wall_bool.wall == 1:
+        wall_status = CLOSED
+    else:
+        wall_status = OPEN
+    known_maze.set_cell_wall(col, row, direction, wall_status)
+
+
+    
 def initialize_commands():
     rospy.init_node('mazesolvernode', anonymous=True)
     rospy.wait_for_service('make_maze')
     rospy.wait_for_service('print_maze')
     rospy.wait_for_service('get_wall')
-#    rospy.wait_for_service('constant_command')
+
 
     global make_maze_service, print_maze_service, get_wall_service
-    global constant_command_service
-
     make_maze_service = rospy.ServiceProxy('make_maze', MakeNewMaze)
     print_maze_service = rospy.ServiceProxy('print_maze', Empty)
     get_wall_service = rospy.ServiceProxy('get_wall', GetMazeWall)
-#    constant_command_service = rospy.ServiceProxy('constant_command', ConstantCommand)
+    #make_maze_service(5, 5)
+    for i in range(5):
+        for j in range(5):
+            known_maze.warp_to_position((j, i))
+            update_room()
+    print known_maze.print_maze()
+    move_to_position_generation((0,0))
 
-    make_maze_service(5,5)
-    print_maze_service()
-    print get_wall_service(0,0, UP)
-    solve_maze()
 
-     
+def move_to_position_generation(end_pos):
+    """Generates paths in the known maze."""
+    trace = []
+    stack = []
+    visited = []
+    cur_pos = known_maze.get_rosie_position()     
+    # 0 is previous, 1 is current
+    stack.append((cur_pos, cur_pos))
+    while len(stack) > 0:
+        cur_pos = stack.pop()
+        if cur_pos[1] in visited:
+            continue
+        else:
+            visited.append(cur_pos[1])
+        # Clear until we get back
+        while len(trace) > 1 and trace[-1][1] is not cur_pos[0]:
+            trace.pop()
+
+        trace.append(cur_pos)
+        previous_pos, current_pos = cur_pos
+        if current_pos[0] is end_pos[0] and current_pos[1] is end_pos[1]:
+            print "Success"
+            break
+        neighbor_positions = known_maze.get_adjacent_connected_positions(current_pos)
+        for neighbor_position in neighbor_positions:
+            stack.append((current_pos, neighbor_position))
+    path = []
+    for node in trace:
+        _, cur_node = node
+        path.append(cur_node)
+    print known_maze.print_path_and_maze(path)
+
 if __name__ == "__main__":   
     try: 
         initialize_commands()
