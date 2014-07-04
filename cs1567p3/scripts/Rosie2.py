4cacd30dd4303f7663aa12078d8c7ec0485ed941
Sheridan Zivanovich <sdzivanovich@gmail.com>
2014-07-03 22:19:16 -0400

    Completed wall-following functionality, needs fine-tuning

--- cs1567p3/scripts/Rosie2.py
+++ cs1567p3/scripts/Rosie2.py
@@ -1,46 +1,52 @@
 #!/usr/bin/env python
 
+
 import rospy
 from std_msgs.msg import *
 from nav_msgs.msg import *
 from geometry_msgs.msg import *
+from kobuki_msgs.msg import BumperEvent
 from cs1567p3.srv import *
 from math import fabs, sqrt, asin, degrees
 from functools import partial
 from time import clock
+from threading import *
 
 def orientation_to_theta(orientation):
+    """Takes an orientation object and outputs the z component as degrees"""
     theta = degrees(2 * asin(orientation.z))
     if (theta < 0):
         theta += 360
     return theta
 
 def linear_action(forward, cmd):
-    cmd.linear.x = 0.2
+    """Action to apply to move linearly"""
+    cmd.linear.x = 0.1
     if not forward:
         cmd.linear.x *= -1
 
 def rotate_action(counter_clock, cmd):
-    cmd.angular.z = 0.5
+    """Action to apply to rotate"""
+    cmd.angular.z = 0.6
     if not counter_clock:    
         cmd.angular.z *= -1 
 
 def null_action(cmd):
+    """Action to apply to do nothing"""
     cmd.angular.z = 0.0
     cmd.linear.x = 0.0
 
-def round_to(x, to = 1):
-    return to * round(float(x) / to)
-
 class Pursuit(object):
     def __init__(self):
-        self.forward = True
         self.target = 0
         self.action = None
 
     def update(self, pose):
         """Updates goal, returns true if goal has been met"""
         print "Implement me!"
+
+    def start(self, pose):
+        pass
 
 class DistancePursuit(Pursuit):
     def __init__(self):
@@ -59,6 +65,9 @@ class RotatePursuit(Pursuit):
             return diff < 0.25 or fabs(diff - 360) < 0.25
         # otherwise just check against (target - eps, target + eps)
         return diff < .4
+    def start(self, pose):
+        pass
+        
 
 class NullPursuit(Pursuit):
     def __init__(self):
@@ -75,23 +84,36 @@ class NullPursuit(Pursuit):
         return delta_time > self.target
         
 class Rosie(object):
-    def __init__(self):        
+    ROTATE_DELTA = 25
+    def __init__(self):
+        self.lock = Lock()        
         self.pursuits = []
         self.pose = None
-        self.turns = 0
-        self.error = 1
+        self.start_time = None
+        self.start_pose= None
 
         rospy.init_node('Rosie', anonymous=True)
         rospy.wait_for_service('constant_command')
-        rospy.wait_for_service('rosie_cmd')
 
         self.clear_odometry()
         self.odometry_sub = rospy.Subscriber('/odom', Odometry, self.odometry_callback)
-        self.get_next_cmd = rospy.ServiceProxy('rosie_cmd', GetCommand)
-        self.send_cmd = rospy.ServiceProxy('constant_command', ConstantCommand)
+        self.bumper_sub = rospy.Subscriber('/mobile_base/events/bumper', BumperEvent, self.on_bump)
+        self.const_cmd = rospy.ServiceProxy('constant_command', ConstantCommand)
 
+
+    def send_cmd(self, cmd):
+        self.const_cmd(cmd)
+    
+            
     def start(self):
-        rospy.spin()
+        self.start_time = clock()
+        self.move_then_probe();
+
+    def move_then_probe(self):
+        self.move(0.5)
+        self.rotate(-1 * Rosie.ROTATE_DELTA)
+        #self.move(1000)
+        
 
     def clear_odometry(self):
         pub = rospy.Publisher('/mobile_base/commands/reset_odometry', Empty, queue_size=10)
@@ -108,86 +130,84 @@ class Rosie(object):
             diff = curr_theta - prev_theta
         else:
             diff = prev_theta - curr_theta
-        print diff,"DURRRRR"
         if diff < 0: # flipped around circle
             diff += 360
-        print diff, "HUUURRRR"
         return diff
 
+    def move(self, dist):
+        goal = DistancePursuit()
+        goal.target = fabs(dist)
+        goal.step = lambda pose: sqrt( fabs(self.pose.position.x - pose.position.x)**2 + fabs(self.pose.position.y - pose.position.y)**2)
+        goal.action = partial(linear_action, dist >= 0)
+        self.pursuits.append(goal)
+        self.pursuits.append(NullPursuit())
 
-    def plan_goal(self, goal):
-        epsilon = 0.01
-        if fabs(goal.delta_x - 0.0) < epsilon and fabs(goal.delta_y - 0.0) < epsilon and fabs(goal.delta_theta - 0.0) < epsilon:
-            return False
-
-        if fabs(goal.delta_x - 0.0) > epsilon:
-            x_goal = DistancePursuit()
-            #x_goal.target = fabs(round_to(self.pose.position.x + goal.delta_x, 0.5) - self.pose.position.x)
-            #print "X: ", x_goal.target
-            x_goal.target = fabs(goal.delta_x)
-            x_goal.step = lambda pose: sqrt( fabs(self.pose.position.x - pose.position.x)**2 + fabs(self.pose.position.y - pose.position.y)**2)
-            #x_goal.step = lambda pose: fabs(pose.position.x - self.pose.position.x)
-            x_goal.action = partial(linear_action, goal.delta_x >= 0)
-            self.pursuits.append(x_goal)
-            self.pursuits.append(NullPursuit())
-            return True
-        
-        if fabs(goal.delta_y - 0.0) > epsilon:
-            y_goal = DistancePursuit()
-            #y_goal.target = fabs(round_to(self.pose.position.y + goal.delta_y, 0.5) - self.pose.position.y)
-            #print "Y: ", y_goal.target
-            y_goal.target = fabs(goal.delta_y)
-            #y_goal.step = lambda pose: fabs(pose.position.y - self.pose.position.y)
-            y_goal.step = lambda pose: sqrt( fabs(self.pose.position.x - pose.position.x)**2 + fabs(self.pose.position.y - pose.position.y)**2)
-            y_goal.action = partial(linear_action, goal.delta_y >= 0)
-            self.pursuits.append(y_goal)
-            self.pursuits.append(NullPursuit())
-            return True
-
-        if fabs(goal.delta_theta - 0.0) < epsilon:
-            return False
-
+    def rotate(self, theta):
         theta_goal = RotatePursuit()
-        delta_theta = fabs(goal.delta_theta) % 360
-        if goal.delta_theta < 0:
+        delta_theta = fabs(theta) % 360
+        if theta < 0:
             delta_theta *= -1
         
         theta_goal.target = orientation_to_theta(self.pose.orientation) + delta_theta
 
-        if goal.delta_theta >= 0: 
+        if theta >= 0: 
             # positive theta, so run around 360
             theta_goal.target = theta_goal.target % 360
             
         elif theta_goal.target < 0:
             # run around 360 opposite way
-            theta_goal.target = 360 + theta_goal.target
+            theta_goal.target = 360 + theta_goal.target;
 
-        total_error = self.turns * self.error
+        print "theta_goal.target: ", theta_goal.target
 
-        theta_goal.target = round_to(theta_goal.target - total_error, 90)
-        
-        theta_goal.target = (theta_goal.target + total_error) % 360
-        print "Target: ", theta_goal.target
-        print "Number turns: ", self.turns
-        print "Total error: ", self.turns * self.error
-        self.turns += 1
-        if (self.turns > 30):
-            self.error = 0.9
-
-        theta_goal.action = partial(rotate_action, goal.delta_theta >= 0)
+        theta_goal.action = partial(rotate_action, theta >= 0)
         self.pursuits.append(theta_goal)
         self.pursuits.append(NullPursuit())
 
-        return True
+    def stop(self):
+        self.pursuits = [NullPursuit()]
 
+    def on_bump(self, bump):
+        if bump.state is 1:
+            self.lock.acquire()
+            # stop rosie
+            self.stop()
+            # move backward
+            self.move(-0.1)
+            # rotate away from wall
+            self.rotate(Rosie.ROTATE_DELTA )
+            self.lock.release()
 
+    def is_done(self, pose):
+        if self.start_time is None or self.start_pose is None:
+            return False
+        
+        pose_epsilon = 0.2
+        startup_time = 2
+        x_diff = fabs(self.start_pose.position.x - pose.position.x)
+        y_diff = fabs(self.start_pose.position.y - pose.position.y)
+        t_diff = clock() - self.start_time
+        print "x_diff: ", x_diff, " y_diff: ", y_diff, " t_diff: ", t_diff
+
+        return (x_diff < pose_epsilon and y_diff < pose_epsilon and t_diff > startup_time)
+            
     def odometry_callback(self, odom):
+        pose = odom.pose.pose
         if self.pose is None:
-            self.pose = odom.pose.pose
+            self.start_pose = pose
+            self.pose = pose
+            self.start()
+            return
 
-        #print self.pose
+        if self.is_done(pose):
+            self.lock.acquire()
+            self.stop()
+            self.lock.release()
+
+        #print "Pose: ", self.pose
         command = Twist()
         # see if we have any more goals to execute
+        self.lock.acquire()
         if self.pursuits:
             
             pursuit = self.pursuits[0]
@@ -197,14 +217,13 @@ class Rosie(object):
             if not pursuit.update(odom.pose.pose):
                 pursuit.action(command)
             else:
-                print odom.pose.pose
                 self.pursuits.pop(0)
-            
-            #print "Target: {0}".format(pursuit.target)
-        # no pursuits, grab and plan the next goal
         else:
-            next_goal = self.get_next_cmd()
-            self.plan_goal(next_goal)
+            self.move_then_probe()
+     
+        self.lock.release()
+
+        #print "Theta: ", orientation_to_theta(odom.pose.pose.orientation)
 
         # update pose and send command
         self.pose = odom.pose.pose
@@ -214,5 +233,5 @@ class Rosie(object):
 if __name__ == "__main__":
     try:
         rosie = Rosie()
-        rosie.start()
+        rospy.spin()
     except rospy.ROSInterruptException: pass
